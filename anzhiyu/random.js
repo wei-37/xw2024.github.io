fd21e3b4f7df4727281dc3f3ee7f9405f6f81a59
wei-37 <zhouwei37@126.com>
2024-12-22 04:50:27 +0800

    Site updated: 2024-12-22 04:50:26

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,56 @@
-var posts=["2024/12/22/一口氣看完【清潭國際高中】1~10集完整版 全集電視劇解說，窮學生來到貴族學校。爲了錢全脫了，便宜了渣男/","2024/12/22/台湾TVBS新闻直播/","2024/12/22/一口气看完经典历史神剧《大明王朝1566》/","2024/12/22/post-title/","2024/12/22/熊猫计划/"];function toRandomPost(){
+var posts=["2024/12/22/一口气看完经典历史神剧《大明王朝1566》/","2024/12/22/post-title/","2024/12/22/一口氣看完【清潭國際高中】1~10集完整版 全集電視劇解說，窮學生來到貴族學校。爲了錢全脫了，便宜了渣男/","2024/12/22/台湾TVBS新闻直播/","2024/12/22/熊猫计划/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
-  };
+  };var friend_link_list=[];
+    var refreshNum = 1;
+    function friendChainRandomTransmission() {
+      const randomIndex = Math.floor(Math.random() * friend_link_list.length);
+      const { name, link } = friend_link_list.splice(randomIndex, 1)[0];
+      Snackbar.show({
+        text:
+          "点击前往按钮进入随机一个友链，不保证跳转网站的安全性和可用性。本次随机到的是本站友链：「" + name + "」",
+        duration: 8000,
+        pos: "top-center",
+        actionText: "前往",
+        onActionClick: function (element) {
+          element.style.opacity = 0;
+          window.open(link, "_blank");
+        },
+      });
+    }
+    function addFriendLinksInFooter() {
+      var footerRandomFriendsBtn = document.getElementById("footer-random-friends-btn");
+      if(!footerRandomFriendsBtn) return;
+      footerRandomFriendsBtn.style.opacity = "0.2";
+      footerRandomFriendsBtn.style.transitionDuration = "0.3s";
+      footerRandomFriendsBtn.style.transform = "rotate(" + 360 * refreshNum++ + "deg)";
+      const finalLinkList = [];
+  
+      let count = 0;
+
+      while (friend_link_list.length && count < 3) {
+        const randomIndex = Math.floor(Math.random() * friend_link_list.length);
+        const { name, link, avatar } = friend_link_list.splice(randomIndex, 1)[0];
+  
+        finalLinkList.push({
+          name,
+          link,
+          avatar,
+        });
+        count++;
+      }
+  
+      let html = finalLinkList
+        .map(({ name, link }) => {
+          const returnInfo = "<a class='footer-item' href='" + link + "' target='_blank' rel='noopener nofollow'>" + name + "</a>"
+          return returnInfo;
+        })
+        .join("");
+  
+      html += "<a class='footer-item' href='/link/'>更多</a>";
+
+      document.getElementById("friend-links-in-footer").innerHTML = html;
+
+      setTimeout(()=>{
+        footerRandomFriendsBtn.style.opacity = "1";
+      }, 300)
+    };
